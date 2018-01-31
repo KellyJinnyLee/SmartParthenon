d26ed7fe223deb6411996121a38d33875d6f46e6
HyeonjinLee <21500585@handong.edu>
2018-01-31 17:00:13 +0900

    Parthenon.unity

---
+++ Assets/ParthenonBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ParthenonBuilder : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject cylinderPrefab;
+    public float floorWidth = 5.0f;
+    public float floorDepth = 10.0f;
+    public float floorHeight = 0.25f;
+    public float pillarRadius = 0.2f;
+    public float pillarHeight = 2.0f;
+    public float pillarCountWidth = 6.0f;
+    public float pillarCountDepth = 10.0f;
+    public float roofHeight = 1f;
+    public Material floorMaterial;
+    public Material pillarMaterial;
+    public Material roofMaterial;
+
+    [ContextMenu("Build")]
+    void BuildPrefab()
+    {
+
+        DestroyAll();
+
+        var newGameobject = Instantiate(cubePrefab, transform);
+        var tr = newGameobject.transform;
+        tr.localScale = new Vector3(floorWidth, floorHeight, floorDepth);
+        tr.position = new Vector3(0, 0, 0);
+
+        var newGameobject2 = Instantiate(cubePrefab, transform);
+        var tr2 = newGameobject2.transform;
+        tr2.localScale = new Vector3(floorWidth * 0.9f, floorHeight, floorDepth * 0.9f);
+        tr2.position = new Vector3(0, floorHeight, 0);
+
+        var newGameobject3 = Instantiate(cubePrefab, transform);
+        var tr3 = newGameobject3.transform;
+        tr3.localScale = new Vector3(floorWidth * 0.9f * 0.9f, floorHeight, floorDepth * 0.9f * 0.9f);
+        tr3.position = new Vector3(0, floorHeight*2f, 0);
+
+
+    }
+
+    [ContextMenu("Destroy All")]
+    void DestroyAll()
+    {
+        foreach (Transform t in transform.Cast<Transform>().ToArray())
+        {
+            DestroyImmediate(t.gameObject);
+        }
+    }
+}
